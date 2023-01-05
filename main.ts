0d4f7e276c636f66c1f457854ea896543eaad2d4
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2023-01-05 13:46:35 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+led.enable(false)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+})
